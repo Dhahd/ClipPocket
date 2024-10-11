15da8bd2edcde315ace1024aca05081e61e407ab
Shaneen Dhahd <hadidhahds@gmail.com>
2024-10-11 04:33:00 +0300

    Initial Commit

---
+++ ClipPocket/ClipPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipPocketApp.swift
+//  ClipPocket
+//
+//  Created by Shaneen on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipPocketTests/ClipPocketTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipPocketTests.swift
+//  ClipPocketTests
+//
+//  Created by Shaneen on 10/11/24.
+//
+
+import Testing
+@testable import ClipPocket
+
+struct ClipPocketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
